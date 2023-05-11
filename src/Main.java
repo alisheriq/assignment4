8936487accc5c6ea93076a4a8fc140e15126dacb
admin <eeoneguifeed@gmail.com>
2023-05-11 20:45:33 +0600

    Testing of 10000 preparation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         /*MyHashTable myHashTable = new MyHashTable();
@@ -10,5 +12,15 @@ public class Main {
         System.out.println(myHashTable.contains("Kazakhstan"));
         System.out.println(myHashTable.get("England"));
         System.out.println(myHashTable.getKey("Geneva"));*/
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10000; i++) {
+            int x = random.nextInt(1000);
+            int y = random.nextInt(1000);
+            MyTestingClass key = new MyTestingClass(x, y);
+            Student value = new Student("Student " + i, i);
+            table.put(key, value);
+        }
     }
 }
--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,2 +1,10 @@
 public class MyTestingClass {
+    private int x;
+    private int y;
+
+    public MyTestingClass(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
