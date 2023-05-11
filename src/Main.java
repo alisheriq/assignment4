bea471b5c46afc6646375419687deee5215a3049
admin <eeoneguifeed@gmail.com>
2023-05-11 20:42:14 +0600

    classes for testing created.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyHashTable myHashTable = new MyHashTable();
+        /*MyHashTable myHashTable = new MyHashTable();
         myHashTable.put("France", "Paris");
         myHashTable.put("England", "London");
         myHashTable.put("Switzerland", "Geneva");
@@ -9,6 +9,6 @@ public class Main {
         System.out.println(myHashTable.get("France"));
         System.out.println(myHashTable.contains("Kazakhstan"));
         System.out.println(myHashTable.get("England"));
-        System.out.println(myHashTable.getKey("Geneva"));
+        System.out.println(myHashTable.getKey("Geneva"));*/
     }
 }
