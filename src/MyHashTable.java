1ddd64b60d764c57bf2927fa4e9396c51a7053d5
admin <eeoneguifeed@gmail.com>
2023-05-11 20:37:53 +0600

    get() method created.

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -49,4 +49,15 @@ public class MyHashTable<K,V> {
         table[index] = newNode;
         size++;
     }
+    public V get(K key){
+        int index = hash(key);
+        HashNode<K,V> node = table[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
+        return null;
+    }
 }
