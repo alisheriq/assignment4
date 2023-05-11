fce0ec81b77e7fa21bfcfb38da061faf27f05fd9
admin <eeoneguifeed@gmail.com>
2023-05-11 20:40:47 +0600

    methods tested.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        MyHashTable myHashTable = new MyHashTable();
+        myHashTable.put("France", "Paris");
+        myHashTable.put("England", "London");
+        myHashTable.put("Switzerland", "Geneva");
+        myHashTable.put("Mexico", "Mexico");
+        myHashTable.remove("England");
+        System.out.println(myHashTable.get("France"));
+        System.out.println(myHashTable.contains("Kazakhstan"));
+        System.out.println(myHashTable.get("England"));
+        System.out.println(myHashTable.getKey("Geneva"));
     }
 }
